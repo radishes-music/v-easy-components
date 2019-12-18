e117fcf7a21a3682213678d16258cc2e0f8ca2ac
Link <link19970507@gmail.com>
2019-12-18 14:45:34 +0800

    test: --single-run to ci

--- test/specs/message.spec.js
+++ test/specs/message.spec.js
@@ -1,12 +1,18 @@
 import { Message } from "lib/v-easy-components.common"
 
 describe('Message', () => {
-  it('should message dom', () => {
+  it('should message dom', (done) => {
     Message({
-      message: 'Link'
+      message: 'Link',
+      duration: 500
     })
     const message = document.querySelector('.message p')
-    expect(document.querySelector('.message')).to.exist;
+    expect(document.querySelector('.message')).to.exist
     expect(message.textContent).to.equal('Link')
+
+    setTimeout(() => {
+      expect(document.querySelector('.message')).to.not.exist
+      done()
+    }, 1000)
   })
 })
