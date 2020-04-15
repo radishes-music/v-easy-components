640dea3dca977561435eb9e619b5df564aa1131f
Link <link19970507@gmail.com>
2020-04-15 17:50:33 +0800

    docs: support scroll-bar types

---
+++ types/scroll-bar.d.ts
@@ -0,0 +1,10 @@
+import { VEasyComponent } from './component'
+
+/** ScrollBar Component */
+export declare class VeScroll extends VEasyComponent {
+  /** thumb bar size */
+  size: String | Number
+
+  /** Whether to keep showing bar */
+  always: Boolean
+}
