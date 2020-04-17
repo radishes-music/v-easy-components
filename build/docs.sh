987fb75dbe0ca0b881135a86e67528e2c7f454b4
Link <link19970507@gmail.com>
2020-04-17 12:05:47 +0800

    docs: Unzip v-easy

--- build/docs.sh
+++ build/docs.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 # release to linkorg.club
+DATE=$(date "+ %Y%m%d")
 tar -cjvf linkorg-club.tar.bz2 ./v-easy
-scp -o StrictHostKeyChecking=no -r linkorg-club.tar.bz2 ci@112.74.169.178:~/travis-repo
-# ssh ci@112.74.169.178 -o StrictHostKeyChecking=no 'cd ~/travis-repo && tar -xjvf linkorg-club.tar.bz2 -C ~/sitemap'
+scp -o StrictHostKeyChecking=no -r linkorg-club$DATE.tar.bz2 ci@112.74.169.178:~/travis-repo
+ssh ci@112.74.169.178 -o StrictHostKeyChecking=no 'cd ~/travis-repo && tar -xjvf linkorg-club$DATE.tar.bz2 -C ~/sitemap'
 echo 'release success, by'
